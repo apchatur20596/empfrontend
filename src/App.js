cc528cb33885f7c36cec097daac006ae0dae8445
chatur20596 <chatur@usc.edu>
2020-02-28 06:44:39 -0800

    read all employee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table, Button } from 'reactstrap';
 
-function App() {
+class App extends Component {
+
+  state = {
+    employees: []
+  }
+
+  componentWillMount() {
+
+    axios.get('http://127.0.0.1:8000/api/readAllEmp/').then((response) => {
+      console.log(response);
+      console.log(response.data);
+      this.setState({
+        employees : response.data
+      })
+    });
+  }
+
+  render() {
+    let employees = this.state.employees.map((employee) => {
+      return (
+        <tr key={employee.id}>
+            <td>{employee.id}</td>
+            <td>{employee.name}</td>
+            <td>
+              <Button color="success" size="sm" className="mr-2">Read</Button>
+              <Button color="success" size="sm" className="mr-2">Update</Button>
+              <Button color="danger" size="sm">Delete</Button>
+            </td>
+          </tr>
+      )
+    });
+
   return (
-    <div className="App">
+    <div className="App container">
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees}
+        </tbody>
+      </Table>
     </div>
   );
+}
 }
 
 export default App;
